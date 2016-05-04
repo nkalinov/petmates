0457cbc189a63e577294eb291e39e40fdf6bf583
Nikola Kalinov <hello@nkalinov.com>
2016-05-04 22:53:22 +0300

    remove typings

--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/googlemaps/index.d.ts" />
-/// <reference path="main/ambient/leaflet/index.d.ts" />
